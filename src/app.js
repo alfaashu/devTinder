1ba06308eed9f38c23fa5a2eb8acf1621c37dc8a
alfaashu <alfaashu@outlook.com>
2025-07-16 11:06:14 +0530

    exploring GET POST DELETE

--- src/app.js
+++ src/app.js
@@ -2,11 +2,29 @@ const express = require("express")
 
 const app = express()
 
-app.use("/test",(req, res) => {
-    res.send("Hello from the server!")
+app.use("/user", (req,res) => {
+    res.send("hahahahahaha")
 })
 
-app.use("/hello", (req, res) => {
+// This will only handle get call to /user
+app.get("/user", (req,res) => {
+    res.send({firstName: "Akshay", lastName: "Saini"})
+})
+
+app.post("/user", (req,res) => {
+    res.send("Data successfully saved")
+})
+
+app.delete("/user", (req,res) => {
+    res.send("Data successfully deleted")
+})
+
+app.patch("/user", (req, res) => {
+    res.send("Data successfully patched")
+})
+
+// This will match all the HTTP method API calls to /test
+app.use("/test", (req, res) => {
     res.send("Hello route executed");
 })
 
