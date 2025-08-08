3de996873760fbc5ef72eb07251e3c5b96a6ad2c
alfaashu <alfaashu@outlook.com>
2025-08-08 07:58:40 +0530

    GET request

--- src/app.js
+++ src/app.js
@@ -1,21 +1,35 @@
 const express = require("express")
 const connectDB = require("./config/database")
-
 const app = express()
 const User = require("./models/user")
 
+app.use(express.json())
+
 app.post("/signup", async (req, res) => {
-    const user = new User({
-        firstName: "Rohit",
-        lastName: "Sharma",
-        emailId: "rsharma@gmail.com",
-        password: "rohit@123"
-    })
+    const user = new User(req.body);
+    
     try {
         await user.save()
-    res.send("User added successfully")
+        res.send('user added successfully')
     } catch (err) {
-        res.status(400).send("Error saving the user:" + err.message)
+            res.status(400).send("Error saving the user ", err.message)
+        }
+})
+
+
+// Get user by email
+
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.emailId
+
+    try {
+
+        const user = await User.find({emailId: userEmail})
+        res.send(user)
+
+    } catch (err) {
+
+        res.status(400).send("Something went wrong")
     }
 })
 
--- src/homework.md
+++ src/homework.md
@@ -2,7 +2,7 @@ Create a repository
 Initialize the project
 Difference between package.json and package-lock.json
 install express
-creaet a server
+Create a server
 listen to port 7777
 write request handlers for /test, /hello
 install nodemon 
